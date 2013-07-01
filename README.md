680047c743c8f0aa6e293e529bd68a4ddcb49aaa
Jonathan Strickland <djanatyn@gmail.com>
2013-07-01 14:24:16 -0400

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-maze
-====
+# maze
 
-maze generator (and solver, eventually?)
+A clojure maze generator, using depth-first search.
+
+## License
+
+Copyright © 2013 Jonathan Strickland
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject maze "0.1.0-SNAPSHOT"
+  :description "a maze generator"
+  :url "http://github.com/djanatyn/maze"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
